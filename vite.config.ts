2fb16a7f438ccb7a31a4e1bacd765b3484f3185d
Francesco <francescolavoro0209@gmail.com>
2026-06-26 12:44:11 +0200

    Bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/pomodoro-ts/",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        }
+      }
+    }
+  }
 });
